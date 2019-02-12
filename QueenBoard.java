9ca017c4a023d08efd7aca15c35074451ebf902e
Tammy <tchen00@stuy.edu>
2019-02-11 21:25:28 -0500

    finish debugging hopefully :

--- QueenBoard.java
+++ QueenBoard.java
@@ -96,13 +96,13 @@ public class QueenBoard{
 
   // METHOD - toString to visualize the board
   public String toString(){
-    String output="";
+    String output = "";
   	for (int i=0;i<size;i++){
   	  for (int j=0;j<size;j++){
   		  if (board[i][j]!=-1){
-  			  output+="_";
-  		   }else {
-  		    output+="Q";
+  			  output+="_ ";
+  		   } else {
+  		    output+="Q ";
   		    }
   	   } output+="\n";
   	} return output;
@@ -174,6 +174,7 @@ public class QueenBoard{
 
   public int countSolutions() {
     // essentially same concept as solve
+    clear();
     for (int i=0;i<size;i++){
       for (int j=0;j<size;j++){
         if (board[i][j]!=0){
@@ -209,77 +210,10 @@ public class QueenBoard{
     }
   }
   public static void main(String[] args){
-    //christys driver
-    QueenBoard board1 = new QueenBoard(1);
-        QueenBoard board2 = new QueenBoard(2);
-        QueenBoard board3 = new QueenBoard(3);
-        QueenBoard board4 = new QueenBoard(4);
-        QueenBoard board5 = new QueenBoard(5);
-        QueenBoard board6 = new QueenBoard(6);
-        QueenBoard board7 = new QueenBoard(7);
-        QueenBoard board8 = new QueenBoard(8);
-
-        System.out.println("");
-
-        //System.out.println(board4.isEmpty());//should print true
-
-        System.out.println("");
-
-        System.out.println(board4);//should print 4x4 board
-
-        System.out.println("");
-
-        board1.solve();
-        System.out.println(board1);
-        System.out.println("");
-
-        board2.solve();
-        System.out.println(board2);
-        System.out.println("");
-
-        board3.solve();
-        System.out.println(board3);
-        System.out.println("");
-
-        board4.solve();
-        System.out.println(board4);
-        System.out.println("");
-
-        board5.solve();
-        System.out.println(board5);
-        System.out.println("");
-
-        board6.solve();
-        System.out.println(board6);
-        System.out.println("");
-
-        board7.solve();
-        System.out.println(board7);
-        System.out.println("");
-
-        board8.solve();
-        System.out.println(board8);
-        System.out.println("");
-
-/*
-        //testing out count
-        try {
-          System.out.println(board8.countSolutions());
-        }
-        catch (IllegalStateException e){
-          System.out.println("board is not empty");
-        }
-*/
-        System.out.println("");
-        System.out.println("");
-
-        System.out.println(board1.countSolutions());//1
-        System.out.println(board2.countSolutions());//0
-        System.out.println(board3.countSolutions());//0
-        System.out.println(board4.countSolutions());//2
-        System.out.println(board5.countSolutions());//10
-        System.out.println(board6.countSolutions());//4
-        System.out.println(board7.countSolutions());//40
-        System.out.println(board8.countSolutions());//92
+    QueenBoard board1 = new QueenBoard(10);
+    board1.solve();
+    System.out.println(board1);
+    System.out.println(board1.countSolutions());
+    //System.out.println(board1);
   }
 }
