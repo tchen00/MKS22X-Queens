e3b7851c085a8a809f9939213996a600fba41fba
Tammy <tchen00@stuy.edu>
2019-02-26 19:20:31 -0500

    add driver

--- QueenBoard.java
+++ QueenBoard.java
@@ -209,11 +209,27 @@ public class QueenBoard{
       }
     }
   }
-  public static void main(String[] args){
-    QueenBoard board1 = new QueenBoard(10);
-    board1.solve();
-    System.out.println(board1);
-    System.out.println(board1.countSolutions());
-    //System.out.println(board1);
+  public static void runTest(int i){
+          QueenBoard b;
+          int[]tests =   {1,2,3,4,5,8};
+          int[]answers = {1,0,0,2,10,92};
+          if(i >= 0 && i < tests.length ){
+                  int size = tests[i];
+                  int correct = answers[i];
+                  b = new QueenBoard(size);
+                  int ans  = b.countSolutions();
+
+          if(correct==ans){
+                  System.out.println("PASS board size: "+tests[i]+" "+ans);
+          }else{
+                  System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+          }
+          }
+}
+
+  public static void main(String[] args) {
+          for (int i = 0; i < 6; i++) {
+                  runTest(i);
+          }
   }
 }
